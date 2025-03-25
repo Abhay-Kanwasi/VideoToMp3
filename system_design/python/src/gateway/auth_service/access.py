c527901df58bd24fb4ce1e180b30ae6d08b5b220
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-03-25 12:42:48 +0530

    Gateway added convertor added rabbitme added

--- system_design/python/src/gateway/auth_service/access.py
+++ system_design/python/src/gateway/auth_service/access.py
@@ -8,7 +8,7 @@ def login(request):
     basicAuth = (auth.username, auth.password)
 
     response = requests.post(
-        f"htto://{os.environ.get("AUTH_SVC_ADDRESS")}/login", auth = basicAuth
+        f"htto://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth = basicAuth
     ) 
 
     if response.status_code == 200:
