3bd2f899f06f9c71f45e1a94ef8c15963253a57a
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-03-25 19:10:25 +0530

    server code updated

--- system_design/python/src/gateway/server.py
+++ system_design/python/src/gateway/server.py
@@ -1,21 +1,25 @@
 import os, gridfs, pika, json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_service import access
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(server)
 
-fs = gridfs.GridFS(mongo.db)
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
-channel = connection.channel()
+rabbitmq_channel = connection.channel()
 
-@server.router("/login", methods=["POST"])
+@server.route("/login", methods=["POST"])
 def login():
     token, err = access.login(request)
 
@@ -33,7 +37,7 @@ def upload():
         if len(request.files) > 1 or len(request.files) < 1:
             return "Exactly 1 file required", 400
         for key, value  in request.files.items():
-            error = util.upload(file, gridfs_instance, rabbitmq_channel, access)   
+            error = util.upload(value, fs_videos, rabbitmq_channel, access)   
 
             if error:
                 return error
@@ -43,7 +47,26 @@ def upload():
 
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "File id is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "Internal server error", 500
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=80)
